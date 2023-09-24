58f147638a34b942ddfe73584c20ed99faaac296
fquach <fionna.quach.821@gmail.com>
2023-09-23 20:52:37 -0700

    added comments

--- bin/wait/Wait.cpp
+++ bin/wait/Wait.cpp
@@ -11,8 +11,8 @@
 Wait::Wait(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
-    parser().setDescription("Suspend the execution of the thread until a child specified by pid argument has changed state");
-    parser().registerPositional("PROCESS_ID", "Suspend execution until the given pid argument changes state");
+    parser().setDescription("Suspend the execution until a process specified by pid argument has changed state");
+    parser().registerPositional("PROCESS_ID", "Suspend execution until the given pid process changes state");
 }
 
 Wait::~Wait() {}
@@ -22,16 +22,18 @@ Wait::Result Wait::exec()
     ProcessID arg_id;
     int store_result, success;
 
+    //Look for process with specified PID
     if ((arg_id = atoi(arguments().get("PROCESS_ID"))) <= 3) {
         ERROR("Invalid PID `" << arguments().get("PROCESS_ID") <<"`");
         return InvalidArgument;
     }
 
+    //Found valid process, Wait
     success = waitpid(arg_id, &store_result, 0);
 
     if(!success)
     {
-        ERROR("No process of ID " << arguments().get("PROCESS_ID") << " is found.");
+        ERROR("No process of ID " << arguments().get("PROCESS_ID") << " was found.");
         return TimedOut;
     }
 
